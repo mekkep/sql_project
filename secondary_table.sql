9e08e63bf4f9aad5cdb0984f3272918c93866efc
Misa Peslova <131295942+mekkep@users.noreply.github.com>
2023-06-07 11:16:15 +0200

    Update secondary_table.sql

--- secondary_table.sql
+++ secondary_table.sql
@@ -1,5 +1,4 @@
-
-/*evropska tabulka*/
+/*european data*/
 CREATE OR REPLACE TABLE t_misa_peslova_project_SQL_secondary_final AS (
 SELECT
 	c.country,
@@ -10,8 +9,8 @@ SELECT
 	e.taxes
 FROM countries c 
 JOIN economies e 
-    on c.country = e.country 
-    and c.independence_date <= e.year
+	on c.country = e.country 
+	and c.independence_date <= e.year
 	and continent = 'Europe'
 	AND e.YEAR BETWEEN 2006 AND 2018
 GROUP BY c.country,
@@ -22,7 +21,6 @@ GROUP BY c.country,
 	e.taxes
 )
 ;
-
 SELECT*
 FROM t_misa_peslova_project_SQL_secondary_final 
 ;
