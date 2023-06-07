7597c8639dda838d2a4f5a35756cc89f23c6022f
Misa Peslova <131295942+mekkep@users.noreply.github.com>
2023-06-07 11:13:12 +0200

    Update 5.task.sql

--- 5.task.sql
+++ 5.task.sql
@@ -1,13 +1,12 @@
- 
 SELECT
 	Year,
 	GDP_growth,
-	price_growth,
-	wages_growth
+	ROUND(price_growth,2),
+	ROUND(wages_growth,2)
 FROM (
 	SELECT 
 		s1.Year,
-		ROUND((s1.GDP - s2.GDP)/s2.GDP *100,2) as GDP_growth
+		ROUND(((s1.GDP - s2.GDP)/s2.GDP) *100,2) as GDP_growth
 	FROM t_misa_peslova_project_sql_secondary_final s1
 	JOIN t_misa_peslova_project_sql_secondary_final s2
 		ON s1.Year = s2.Year +1
@@ -17,12 +16,14 @@ FROM (
 	JOIN (
 		SELECT
 			v1.payroll_year,
-			ROUND(((AVG(v1.avg_price) - AVG(v2.avg_price))/AVG(v2.avg_price))*100,2) AS price_growth,
-			ROUND(((AVG(v1.average_wages) - AVG(v2.average_wages))/AVG(v2.average_wages))*100,2) AS wages_growth
+			(AVG(v1.avg_price) - AVG(v2.avg_price))/AVG(v2.avg_price)*100 AS price_growth,
+			(AVG(v1.avg_wages) - AVG(v2.avg_wages))/AVG(v2.avg_wages)*100 AS wages_growth
 		FROM t_misa_peslova_project_sql_primary_final v1
 		JOIN t_misa_peslova_project_sql_primary_final v2
 			ON v1.payroll_year = v2.payroll_year+1
 		GROUP BY v1.payroll_year
 		) pw
 	ON gdp.Year = pw.payroll_year
-ORDER BY Year ;
+ORDER BY Year 
+;
+
