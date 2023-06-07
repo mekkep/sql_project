65914e1be23165c0d1ebb7f8cf64075096f8b41f
Misa Peslova <131295942+mekkep@users.noreply.github.com>
2023-06-07 11:09:38 +0200

    Update 2.task.sql

--- 2.task.sql
+++ 2.task.sql
@@ -1,11 +1,10 @@
-
-/* priklad 2*/
-/*prvni obdobi*/ -- 1.q.2006
+/* task 2*/
+/*the first period*/ -- 1st.q.2006
 SELECT MIN(date_from)
 FROM czechia_price cp 
 
 ;
-/*posledni obdobi*/ -- 4.q.2018
+/*the last period*/ -- 4th.q.2018
 SELECT MAX (date_to)
 FROM czechia_price cp 
 
@@ -15,13 +14,13 @@ SELECT
 	payroll_year, 
 	food_category_code,
 	ROUND(avg_price,2) AS price,
-	AVG(average_wages) AS avg_wages_all_branches,
-	FLOOR(average_wages/avg(avg_price)) AS amount_in_units
+	ROUND(AVG(avg_wages),2) AS avg_wages_all_branches,
+	FLOOR(avg_wages/avg(avg_price)) AS amount_in_units
 FROM t_misa_peslova_project_sql_primary_final AS v1
-WHERE food_category_code IN ('111301', '114201')
+WHERE food_category_code IN (111301, 114201)
 	AND payroll_year IN (2006,2018)
-GROUP BY payroll_year, 
-		food_category_code,
-		avg_price
-	
+GROUP BY 
+	payroll_year, 
+	food_category_code,
+	avg_price
 ;
